a0e5b9f105160a8e64afeea046f1095c079c078e
toshiboufishing-hue <toshibou.fishing@gmail.com>
2026-08-25 07:34:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='basslog-v10-6-stage2-bass-stage1';
+const CACHE='basslog-v10-6-stage3-header';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
