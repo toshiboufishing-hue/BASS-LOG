27ae2f57fa34b5f82e41a955260f6c602582bece
toshiboufishing-hue <toshibou.fishing@gmail.com>
2026-08-27 17:48:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Bass Log service worker revision: v10.36
-const BASS_LOG_SW_REVISION = 'v10.36';
+// Bass Log service worker revision: v10.37
+const BASS_LOG_SW_REVISION = 'v10.37';
 const CACHE_NAME='basslog-v10-16-header-redo-20260827';
 const OFFLINE_URL='./index.html';
 
