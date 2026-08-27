f5213d317907ecef64b96183e8d877b60e3eaaee
toshiboufishing-hue <toshibou.fishing@gmail.com>
2026-08-27 13:41:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='basslog-v10-14-logo-balance-20260827';
+const CACHE_NAME='basslog-v10-15-diagonal-logo-20260827';
 const OFFLINE_URL='./index.html';
 
 self.addEventListener('install',event=>{
