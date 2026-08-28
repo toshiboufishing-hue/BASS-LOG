f130b150f7b92b488fb04260041a828b89847e7f
toshiboufishing-hue <toshibou.fishing@gmail.com>
2026-08-29 02:21:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// Bass Log service worker revision: v10.64
-const BASS_LOG_SW_REVISION = 'v10.64';
-const CACHE_NAME='basslog-v10-64-calendar-file-bg-20260829';
+// Bass Log service worker revision: v10.65
+const BASS_LOG_SW_REVISION = 'v10.65';
+const CACHE_NAME='basslog-v10-65-cover-fix-20260829';
 const OFFLINE_URL='./index.html';
 
 self.addEventListener('install',event=>{
